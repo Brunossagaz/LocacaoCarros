81a7a61738f69426aeb2e0caa6ca8013485b3878
Bruno Felipe Sagaz <brunossagaz@gmail.com>
2023-08-27 15:32:50 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Carros.cs
@@ -0,0 +1,14 @@
+﻿namespace LocacaoCarros.Models
+{   
+
+
+    public class Carros
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+        public string Cor { get; set; }
+        public float Valor { get; set; }
+
+    }
+}
